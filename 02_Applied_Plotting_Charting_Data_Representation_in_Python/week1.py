0528959d2457a1ca4cd321a01723fc3ab654c8ae
Jacob Ho <youngjho90@gmail.com>
2021-01-12 22:44:37 +0900

    Started course 2. Week1.

---
+++ 02_Applied_Plotting_Charting_Data_Representation_in_Python/week1.py
@@ -0,0 +1,77 @@
+### Visualization Wheel (Alberto Cairo)
+
+* Abstraction - Figuration
+    - Boxes and charts (abstraction)
+        vs
+    - Real-world physical objects (figuration)
+
+* Functionality - Decoration
+    - No embellishments (functionality)
+        vs
+    - Artistic embellishments (decoration)
+
+* Density - Lightness
+    - Must be studied in depth (density)
+        vs
+    - Understandable at a glance (lightness)
+
+* Multidimensional - Unidimensional
+    - Different aspects of phenomena (multidimensional)
+        vs
+    - Single or few items of phenomena (unidimensional)
+
+* Originality - Familiarity
+    - Novel methods of visualization (originality)
+        vs
+    - Established and well understood methods of visualization (familiarity)
+
+* Novelty - Redundancy
+    - Explaining each item once (novelty)
+        vs
+    - Encoding multiple explanations of the same phenomena (redundancy)
+
+### Graphical heuristics : Data-ink ratio (Edward Tufte)
+* What is heuristics?
+    : process or a rule that is meant to guide you in decision making.
+    : not optimal or perfect but 'practical'
+
+* Data-ink ratio
+    : remove unnecessary 'ink' (information) => more simpler and readable
+    : but increase the amount of information the viewer sees
+
+* ex: Dark Horse Analytics
+
+### Graphical heuristics : Chart junk (Edward Tufte)
+* 3 kinds of chart junk
+    1. Unintended optical art
+        : patterns in graphs etc
+        : rather just label them directly
+    2. Grid
+        : unnecessary
+    3. duck
+        : non-data creative graphics
+
+### Graphical heuristics : Lie Factor and Spark Lines (Edward Tufte)
+* Spark Lines
+    : a small graph, say a time series graph
+    : 'data words'
+    : intriguing way to bridge the gap between text and figures
+
+* Lie Factor
+    : size of an effect shown in the graphic divided by,
+      the size of the effect actually in the data
+
+### The Truthful Art (Alberto Cairo)
+* 5 part framework for considering the qualities of an information graphic
+    1. truthful 
+        - subjective. But, 
+        - honest with ourselves that we are not obscuring data.
+        - misleading audience
+    2. functionality
+    3. beauty
+        - different perceptions on beauty
+    4. insightful
+        - draw the viewer in for 'a-ha eureka!' moment
+    5. enlightening
+        - different from insightful
+        - combination of previous four, but adds a social responsibility
